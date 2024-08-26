a175c83c06f3ca9d84d09a139c58c81ce64f7659
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2024-08-26 15:50:33 -0300

    Correções no layout e troca de table por carousel

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,14 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
-import { NgxPaginationModule } from 'ngx-pagination';
 import { SkinService } from '../../services/skin.service'; 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    CommonModule,
-    NgxPaginationModule
+    CommonModule
   ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
@@ -16,38 +14,30 @@ import { SkinService } from '../../services/skin.service';
 })
 export class LayoutComponent implements OnInit {
   skins: any[] = [];
-  emptySlots: any[] = [
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null },
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null },
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null },
-    { skin: null }, 
-    { skin: null }, 
-    { skin: null },
-  ];
-  page: number = 1;
+  carouselSlides: any[][] = [];
+  currentSlide: number = 0;
+  slidesCount: number = 0;
+  emptySlots: any[] = Array.from({ length: 15 }, () => ({ skin: null }));
   loading: boolean = true;
   draggedSkin: any = null;
 
   constructor(private skinService: SkinService) {}
 
   ngOnInit(): void {
-    this.loading = true;
     this.skinService.getSkins().subscribe(
       (data: any[]) => {
         if (data) {
+          this.loading = true;
           this.skins = data;
+          this.slidesCount = Math.ceil(this.skins.length / 16);
+
+          // Carregar o primeiro slide
+          this.loadSlide(this.currentSlide);
+
           this.loading = false;
         } else {
           console.error('Dados inválidos recebidos da API:', data);
+          this.loading = false;
         }
       },
       (error) => {
@@ -55,6 +45,27 @@ export class LayoutComponent implements OnInit {
         this.loading = false;
       }
     );
+  }
+
+  loadSlide(slideIndex: number): void {
+    // Carregar o slide apenas se ainda não estiver carregado
+    if (!this.carouselSlides[slideIndex]) {
+      const start = slideIndex * 16;
+      const end = start + 16;
+      this.carouselSlides[slideIndex] = this.skins.slice(start, end);
+    }
+  }
+
+  prevSlide(): void {
+    // Muda para o slide anterior e carrega o slide
+    this.currentSlide = (this.currentSlide > 0) ? this.currentSlide - 1 : this.slidesCount - 1;
+    this.loadSlide(this.currentSlide);
+  }
+
+  nextSlide(): void {
+    // Muda para o próximo slide e carrega o slide
+    this.currentSlide = (this.currentSlide < this.slidesCount - 1) ? this.currentSlide + 1 : 0;
+    this.loadSlide(this.currentSlide);
   }
 
   onDragStart(event: DragEvent, skin: any): void {
